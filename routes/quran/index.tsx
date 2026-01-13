b071a5dc8c685c707dac816352fee17d749f8110
Muhaimin CS <muhaimincs@gmail.com>
2026-01-13 23:50:11 +0800

    fix: adjust padding and layout for verse display in Home component

--- routes/quran/index.tsx
+++ routes/quran/index.tsx
@@ -6,10 +6,10 @@ export default define.page(function Home(ctx) {
   const verses = alfatihah?.verses || [];
 
   return (
-    <div class="mx-auto max-w-5xl px-6 py-3 lg:px-8">
+    <div class="mx-auto max-w-5xl px-0 py-3 lg:px-8">
       <dl class="mt-20 divide-y divide-gray-900/10">
         {verses.map((verse) => (
-          <div key={verse.id} class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+          <div key={verse.id} class="py-8 first:pt-0 last:pb-0 flex flex-col-reverse lg:grid lg:grid-cols-12 lg:gap-8">
             <dt dir="ltr" class="text-base/7 font-semibold text-gray-900 lg:col-span-5 ltr:ml-3">
               {verse.id}. {verse.translations?.[0].text}
             </dt>
