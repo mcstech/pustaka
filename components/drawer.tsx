4f6d1d0952b8170d3951cf2ee724b01ae35eccb0
Muhaimin CS <muhaimincs@gmail.com>
2026-01-13 22:06:43 +0800

    refactor: restructure Drawer component to improve sidebar organization

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -16,24 +16,32 @@ export function Drawer({ children, isQuran, data, selected }: DrawerProps) {
         {children}
       </div>
 
-      <div class="drawer-side is-drawer-close:overflow-visible">
-        <label for="quran-drawer" aria-label="close sidebar" class="drawer-overlay" />
-        <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-full is-drawer-open:sm:w-80">
-          <ul class="menu w-full grow">
-            <li class="sm:hidden">
-              <DrawerBookSelector isQuran={isQuran} />
-            </li>
-            <li class="is-drawer-open:-mx-2">
-              <BookToC data={data} selected={selected} />
-            </li>
-            <li class="mt-auto">
-              <button type="button" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
-                <i class="ph-duotone ph-identification-badge text-xl" />
-                <span class="is-drawer-close:hidden">Nama User</span>
-              </button>
-            </li>
-          </ul>
-        </div>
+      <DrawerSidebar isQuran={isQuran} data={data} selected={selected}>
+        <li class="mt-auto">
+          <button type="button" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Settings">
+            <i class="ph-duotone ph-identification-badge text-xl" />
+            <span class="is-drawer-close:hidden">Nama User</span>
+          </button>
+        </li>
+      </DrawerSidebar>
+    </div>
+  )
+}
+
+export function DrawerSidebar({ isQuran, data, selected, children }: DrawerProps) {
+  return (
+    <div class="drawer-side is-drawer-close:overflow-visible">
+      <label for="quran-drawer" aria-label="close sidebar" class="drawer-overlay" />
+      <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-5/6 is-drawer-open:sm:w-80">
+        <ul class="menu w-full grow">
+          <li class="sm:hidden">
+            <DrawerBookSelector isQuran={isQuran} />
+          </li>
+          <li class="is-drawer-open:-mx-2">
+            <BookToC data={data} selected={selected} />
+          </li>
+          {children}
+        </ul>
       </div>
     </div>
   )
