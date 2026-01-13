42144ff6f007189f0c4c83ccfc5bcadcc9cf1ef8
Muhaimin CS <muhaimincs@gmail.com>
2026-01-13 23:27:54 +0800

    feat: implement Quran chapter display with verses in Home component

--- routes/quran/index.tsx
+++ routes/quran/index.tsx
@@ -1,9 +1,26 @@
 import { define } from "@/utils.ts";
 
 export default define.page(function Home(ctx) {
+  const quran = ctx.state.quran;
+  const alfatihah = quran.chapters.find((chapter) => chapter.id === 1);
+  const verses = alfatihah?.verses || [];
+
   return (
-    <>
-      <p>Bismillah</p>
-    </>
+    <div class="mx-auto max-w-5xl px-6 py-3 lg:px-8">
+      <dl class="mt-20 divide-y divide-gray-900/10">
+        {verses.map((verse) => (
+          <div key={verse.id} class="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
+            <dt dir="ltr" class="text-base/7 font-semibold text-gray-900 lg:col-span-5 ltr:ml-3">
+              {verse.id}. {verse.translations?.[0].text}
+            </dt>
+            <dd dir="rtl" class="mt-4 lg:col-span-7 lg:mt-0">
+              <p class="text-base/7 text-gray-600 rtl:mr-3">
+                {verse.text_uthmani}
+              </p>
+            </dd>
+          </div>
+        ))}
+      </dl>
+    </div>
   );
 });
