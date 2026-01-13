cffab31c4470e1084c329b3dd357fd10a4ee9c39
Muhaimin CS <muhaimincs@gmail.com>
2026-01-13 23:27:45 +0800

    fix: update HeaderDrawer styles for improved visual consistency

--- components/header.tsx
+++ components/header.tsx
@@ -20,11 +20,11 @@ export function Header(props: HeaderProps) {
 
 export function HeaderDrawer(props: HeaderProps) {
   return (
-    <nav class={cn("navbar w-full bg-base-300 space-x-3 items-center", props.class)}>
-      <label for="quran-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost btn-sm">
+    <nav class={cn("navbar w-full bg-transparent space-x-3 items-center", props.class)}>
+      <label for="quran-drawer" aria-label="open sidebar" class="btn btn-circle btn-sm drop-shadow-md">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor" class="inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
       </label>
-      <label class="input input-sm">
+      <label class="input input-sm drop-shadow-md">
         <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <g
             stroke-linejoin="round"
