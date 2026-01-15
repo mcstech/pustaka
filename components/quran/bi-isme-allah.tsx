88ebb8030f9576d270bc623bf517899e48740535
Muhaimin CS <muhaimincs@gmail.com>
2026-01-15 22:48:06 +0800

    fix: add bismillah.svg file to static assets

---
+++ components/quran/bi-isme-allah.tsx
@@ -0,0 +1,11 @@
+export function Bismillahirrahmanirrahim() {
+  return (
+    <img
+      class="my-6"
+      src="/bismillah.svg"
+      width="128"
+      height="128"
+      alt="Bismillahirrahmanirrahim"
+    />
+  )
+}
