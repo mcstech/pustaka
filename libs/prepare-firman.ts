1dac45e1cbd7e879e6e8513895bda7fddba5bf04
Muhaimin CS <muhaimincs@gmail.com>
2026-01-18 13:53:37 +0800

    fix: remove unused Alfatiha import and simplify Quran chapter preparation

--- libs/prepare-firman.ts
+++ libs/prepare-firman.ts
@@ -1,30 +1,13 @@
 import { Context } from "fresh";
 import ALQURAN from "@/data/quran/surahs.json" with { type: "json" };
-import ALFATIHAH from "@/data/quran/alfatihah.json" with { type: "json" };
 import BIBLE from "@/data/bible/ind_ayt/books.json" with { type: "json" };
 import GENESIS from "@/data/bible/ind_ayt/gen.json" with { type: "json" };
 import { State } from "@/utils.ts";
-import { Surah, VerseKey } from "@/types/quran/surah.ts";
 
 async function prepareFirman(ctx: Context<State>) {
   const url = ctx.url;
   if (url.pathname.startsWith("/quran")) {
-    const chapters = ALQURAN.chapters.reduce<Surah[]>((acc, chapter) => {
-      if (chapter.id === 1) {
-        acc.push({
-          ...chapter,
-          verses: ALFATIHAH.verses.map((verse) => {
-            return {
-              ...verse,
-              verse_key: verse.verse_key as VerseKey,
-            }
-          }),
-        });
-      } else {
-        acc.push(chapter as unknown as Surah);
-      }
-      return acc;
-    }, []);
+    const chapters = ALQURAN.chapters;
 
     ctx.state.quran = {
       chapters,
