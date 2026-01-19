eb570e3e744a8fd3396a22401ca5f33b8b24dbb0
Muhaimin CS <muhaimincs@gmail.com>
2026-01-19 17:57:39 +0800

    feat: reorganize State interface and add Meta interface for improved structure

--- utils.ts
+++ utils.ts
@@ -2,17 +2,32 @@ import { createDefine } from "fresh";
 import { TranslationBooks } from "./types/bible/books.ts";
 import { Surah } from "./types/quran/surah.ts";
 
-// This specifies the type of "ctx.state" which is used to share
-// data among middlewares, layouts and routes.
-export interface State {
+
+export interface Meta {
   title?: string;
   description?: string;
   ogImage?: string;
+}
+// This specifies the type of "ctx.state" which is used to share
+// data among middlewares, layouts and routes.
+export interface State {
+  meta: Meta;
   noIndex?: boolean;
   quran: {
     chapters: Surah[];
   },
   alkitab: TranslationBooks;
+}
+
+export interface WejanganBMS {
+  css: string;
+  comrakCss: string;
+  script: string;
+  // null only on index page
+  breadcrumbs: string | null;
+  // null only on all symbols page
+  toc: string | null;
+  main: string;
 }
 
 export const define = createDefine<State>();
