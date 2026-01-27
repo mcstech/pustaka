16c24a6fdff7ac68989b6a1bd789407875c922b5
Muhaimin CS <muhaimincs@gmail.com>
2026-01-27 19:36:28 +0800

    feat: enhance Markdown component to support RTL text rendering for Arabic

--- components/markdown.tsx
+++ components/markdown.tsx
@@ -15,13 +15,20 @@ export function Markdown(
     baseUrl: baseURL,
     mediaBaseUrl: mediaBaseURL,
   });
+  
+  // Process HTML to add dir=rtl to pre tags containing Arabic text
+  const processedHtml = html.replace(
+    /<pre>([^<]*[\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDFF\uFE70-\uFEFF][^<]*)<\/pre>/g,
+    '<pre dir="rtl">$1</pre>'
+  );
+  
   return (
     <div
       data-light-theme="light"
       data-dark-theme="dark"
       class="markdown-body prose sm:prose-xl prose-p:text-base-content prose-pre:font-lpmq prose-pre:text-2xl prose-pre:text-end prose-h1:text-3xl prose-h2:text-xl"
       // deno-lint-ignore react-no-danger
-      dangerouslySetInnerHTML={{ __html: html }}
+      dangerouslySetInnerHTML={{ __html: processedHtml }}
     />
   );
 }
