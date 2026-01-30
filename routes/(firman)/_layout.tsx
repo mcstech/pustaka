37d21251593ed3d59acbcc2170ad7f63880bb674
Muhaimin CS <muhaimincs@gmail.com>
2026-01-30 22:02:52 +0800

    fix: add font preloading and stylesheet links in layout component

--- routes/(firman)/_layout.tsx
+++ routes/(firman)/_layout.tsx
@@ -1,3 +1,4 @@
+import { Head, asset} from "fresh/runtime";
 import { Drawer } from "@/components/drawer.tsx";
 import { define } from "@/utils.ts";
 
@@ -18,6 +19,27 @@ export default define.layout((props) => {
 
   return (
     <Drawer isQuran={isQuran} data={listTitles} selected={selected}>
+      <Head>
+        <link
+          rel="preload"
+          href={asset("/fonts/quran/lpmq.woff")}
+          as="font"
+          type="font/woff"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href={asset("/fonts/quran/surah-names/v1/surah-names.woff2")}
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/duotone/style.css"
+        />
+      </Head>
       <Component />
     </Drawer>
   );
