4fdfec287af67e4b27ddbedae032cae0bd04875e
Muhaimin CS <muhaimincs@gmail.com>
2026-02-26 23:04:32 +0800

    feat: add random quotes data for spiritual reflection

---
+++ libs/wejangan/random-quotes.ts
@@ -0,0 +1,10 @@
+import { RandomVerse } from "@/types/index.ts";
+
+export const randomQuotes: RandomVerse[] = [
+  {
+    text: "Tahukah Anda bahwa Allah itu adalah Tuhan Yang Pencemburu. Dia sangat murka kepada hamba-hamba-Nya yang mencintai ilah-ilah (tuan-tuan) yang lain. Dia sangat marah bila hamba-hamba-Nya memiliki sesuatu yang dicintai dan dipatuhi selain kepada-Nya. Namun demikian, berapa banyak manusia yang mencintai musuh-musuh (tandingan-tandingan) Allah seperti mereka mencintai-Nya. Ketika Anda memiliki kecintaan kepada sesuatu melebihi atau menyamai cinta Anda kepada-Nya, maka Anda masih berada dalam kemusyrikan.",
+    reference: "Renungan Spiritual",
+    category: "wejangan",
+    href: "",
+  }
+];
