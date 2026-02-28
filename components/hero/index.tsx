d198a858eedd19c364123bee5a3a3f81e28328d9
Muhaimin CS <muhaimincs@gmail.com>
2026-02-28 10:58:11 +0800

    fix: remove ThemeToggle component from Hero for cleaner layout

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,7 +1,6 @@
 import type { ComponentChildren } from "preact";
 
 import { cn } from "@/libs/utils.ts";
-import { ThemeToggle } from "@/islands/ThemeToggle.tsx";
 
 interface HeroProps {
   class?: string;
@@ -20,11 +19,6 @@ export function Hero(props: HeroProps) {
           style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
           class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-primary-foreground to-primary opacity-10 sm:left-[calc(50%-30rem)] sm:w-288.75"
         />
-      </div>
-
-      {/* Theme toggle in top-right */}
-      <div class="absolute top-0 right-0 m-4 p-1 rounded-full border border-base-300 scale-75 origin-top-right">
-        <ThemeToggle />
       </div>
 
       {/* Centered content */}
