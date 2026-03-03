8531910e9a80d35e40e825f6ebba5f6d8d38c2be
Muhaimin CS <muhaimincs@gmail.com>
2026-03-03 20:51:07 +0800

    fix: add min-width to background div in AtasNamaTuanSemestaAlam component

--- components/anTSA.tsx
+++ components/anTSA.tsx
@@ -1,7 +1,7 @@
 export function AtasNamaTuanSemestaAlam() {
   return (
     <div class="relative grayscale w-full">
-      <div class="absolute -bottom-8 lg:-bottom-12.5 -z-10 h-40 lg:h-64 w-full bg-[url(/logo.png)] bg-no-repeat bg-cover bg-center" />
+      <div class="absolute -bottom-8 lg:-bottom-12.5 -z-10 h-40 min-w-100 lg:h-64 w-full bg-[url(/logo.png)] bg-no-repeat bg-cover bg-center" />
       <h1 class="text-xs font-bold text-center text-shadow-lg text-shadow-accent-content">ATAS NAMA TUAN SEMESTA ALAM</h1>
       <h1 class="text-sm font-bold text-center text-shadow-lg/50 text-shadow-accent-content">YANG MAHA PENGASIH & PENYAYANG</h1>
     </div>
